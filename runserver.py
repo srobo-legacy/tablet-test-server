c3f7f3e7bffcfcbfb444afa270182ff2ac7da298
Tom Leese <tom@tomleese.me.uk>
2014-07-08 10:13:41 +0100

    Fix mode picker

--- runserver.py
+++ runserver.py
@@ -2,12 +2,21 @@
 import flask
 
 
-app = flask.Flask(__name__, template_folder=".", static_folder=".", static_url_path="")
+app = flask.Flask(__name__, template_folder=".", static_folder=".",
+                  static_url_path="")
+mode = "dev"
 
 
 @app.route("/")
 def index():
     return flask.render_template("index.html")
 
+
+@app.route("/settings/mode", methods=["GET", "PUT"])
+def settings_mode():
+    global mode
+    if flask.request.method == "PUT":
+        mode = flask.request.json["mode"]
+    return flask.jsonify(mode=mode)
 
 app.run(port=8000, debug=True)

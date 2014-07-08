c75977d0013b123790433dd9ee8465723cf03e09
Tom Leese <tom@tomleese.me.uk>
2014-07-08 10:35:47 +0100

    Implement getting previous log

--- runserver.py
+++ runserver.py
@@ -9,6 +9,7 @@ app = flask.Flask(__name__, template_folder=".", static_folder=".",
 start_time = time.time()
 mode = "dev"
 zone = 0
+log_list = []
 
 
 @app.route("/")
@@ -20,6 +21,8 @@ def index():
 def settings_mode():
     global mode
     if flask.request.method == "PUT":
+        global log_list
+        log_list.append("Changed mode to: {}".format(mode))
         mode = flask.request.json["mode"]
     return flask.jsonify(mode=mode)
 
@@ -28,6 +31,8 @@ def settings_mode():
 def settings_zone():
     global zone
     if flask.request.method == "PUT":
+        global log_list
+        log_list.append("Changed zone to: {}".format(zone))
         zone = flask.request.json["zone"]
     return flask.jsonify(zone=zone)
 
@@ -37,6 +42,12 @@ def battery():
     global start_time
     level = 1 - ((time.time() - start_time) / 1000)
     return flask.jsonify(level=level)
+
+
+@app.route("/log")
+def log():
+    global log_list
+    return flask.jsonify(log="\n".join(log_list))
 
 
 app.run(port=8000, debug=True)

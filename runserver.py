e1c33ffa879c711295cdd9ced28161669677111e
Tom Leese <tom@tomleese.me.uk>
2014-07-08 10:21:50 +0100

    Fix zone picker AJAX request

--- runserver.py
+++ runserver.py
@@ -5,6 +5,7 @@ import flask
 app = flask.Flask(__name__, template_folder=".", static_folder=".",
                   static_url_path="")
 mode = "dev"
+zone = 0
 
 
 @app.route("/")
@@ -18,5 +19,13 @@ def settings_mode():
     if flask.request.method == "PUT":
         mode = flask.request.json["mode"]
     return flask.jsonify(mode=mode)
+
+
+@app.route("/settings/zone", methods=["GET", "PUT"])
+def settings_zone():
+    global zone
+    if flask.request.method == "PUT":
+        zone = flask.request.json["zone"]
+    return flask.jsonify(zone=zone)
 
 app.run(port=8000, debug=True)

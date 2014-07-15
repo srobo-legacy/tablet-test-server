48095adfc79778cd8b7a569051676e601a4b3f6a
Tom Leese <tom@tomleese.me.uk>
2014-07-15 14:31:46 +0100

    Add custom component loading

--- usercode/app.py
+++ usercode/app.py
@@ -3,22 +3,33 @@ import flask
 import flask_cors
 
 
-app = flask.Flask(__name__)
+class FlaskStaticCors(flask.Flask):
+    @flask_cors.cross_origin()
+    def send_static_file(self, filename):
+        return super(FlaskStaticCors, self).send_static_file(filename)
 
 
-@app.route("/custom_pages")
+app = FlaskStaticCors(__name__)
+
+
+@app.route("/custom_pages/info")
 @flask_cors.cross_origin()
-def custom_pages():
+def custom_pages_info():
     return flask.jsonify(
         pages=[
             {
                 "title": "My Custom Component",
                 "name": "my-custom-component",
-                "icon": "cubes",
-                "url": "/static/elements/my-custom-components/index.html"
+                "icon": "cubes"
             }
         ]
     )
+
+
+@app.route("/custom_pages/import")
+@flask_cors.cross_origin()
+def custom_pages_import():
+    return flask.send_file("static/elements/index.html")
 
 
 if __name__ == "__main__":

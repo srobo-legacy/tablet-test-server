c4cdd1c8df4130b8c56307d51279389cb825ff87
Tom Leese <tom@tomleese.me.uk>
2014-08-15 15:08:38 +0100

    Move WAMP code into seperate class

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ import flask
 import crochet; crochet.setup()
 import twisted.internet
 from autobahn.twisted import wamp
+from twisted.internet.defer import inlineCallbacks
 
 
 g = {
@@ -139,199 +140,153 @@ g = {
 
 ################################################################################
 class MyComponent(wamp.ApplicationSession):
+    @inlineCallbacks
     def onJoin(self, details):
         print(details)
+        yield self.subscribe(self.wapp_hello, "org.srobo.hello")
+        yield self.register(self.wapp_get_zone, "org.srobo.zone")
+        yield self.subscribe(self.wapp_sub_zone, "org.srobo.zone")
+        yield self.register(self.wapp_get_mode, "org.srobo.mode")
+        yield self.subscribe(self.wapp_sub_mode, "org.srobo.mode")
+        yield self.subscribe(self.wapp_sub_log, "org.srobo.logs.append")
+        yield self.register(self.wapp_logs_all, "org.srobo.logs.all")
+        yield self.register(self.wapp_logs_get, "org.srobo.logs.get")
+        yield self.register(self.wapp_start, "org.srobo.start")
+        yield self.register(self.wapp_stop, "org.srobo.stop")
+        yield self.register(self.wapp_get_state, "org.srobo.state")
+        yield self.register(self.wapp_get_pyenv_version, "org.srobo.pyenv.version")
+        yield self.register(self.wapp_get_project_name, "org.srobo.project.name")
+        yield self.register(self.wapp_get_project_version, "org.srobo.project.version")
+        yield self.subscribe(self.wapp_power_output_state, "org.srobo.power.output_state")
+        yield self.register(self.wapp_power_get_output_state, "org.srobo.power.get_output_state")
+        yield self.subscribe(self.wapp_servos_servo_value, "org.srobo.servos.servo_value")
+        yield self.register(self.wapp_servos_get_servo, "org.srobo.servos.get_servo")
+        yield self.register(self.wapp_servos_get_board, "org.srobo.servos.get_board")
+        yield self.register(self.wapp_servos_all_boards, "org.srobo.servos.all_boards")
+        yield self.subscribe(self.wapp_motors_motor_value, "org.srobo.motors.motor_value")
+        yield self.register(self.wapp_motors_get_motor, "org.srobo.motors.get_motor")
+        yield self.register(self.wapp_motors_get_board, "org.srobo.motors.get_board")
+        yield self.register(self.wapp_motors_all_boards, "org.srobo.motors.all_boards")
+        yield self.subscribe(self.wapp_ruggeduinos_pin_mode, "org.srobo.ruggeduinos.pin_mode")
+        yield self.subscribe(self.wapp_ruggeduinos_pin_value, "org.srobo.ruggeduinos.pin_value")
+        yield self.register(self.wapp_ruggeduinos_get_pin, "org.srobo.ruggeduinos.get_pin")
+        yield self.register(self.wapp_ruggeduinos_get_board, "org.srobo.ruggeduinos.get_board")
+        yield self.register(self.wapp_ruggeduinos_all_boards, "org.srobo.ruggeduinos.all_boards")
+
+    def find_log(self, name):
+        for log in g["logs"]:
+            if log["name"] == name:
+                return log
+
+    def log(self, m):
+        self.publish("org.srobo.logs.append", "current", m)
+        self.find_log("current")["contents"].append(m)
+
+    def wapp_hello(self, client_version):
+        compatible = False if random.randint(0, 10) == 0 else True
+        return {"compatible": compatible}
+
+    def wapp_get_zone(self):
+        return g["zone"]
+
+    def wapp_sub_zone(self, zone):
+        self.log("Zone changed to: {}".format(zone))
+        g["zone"] = zone
+
+    def wapp_get_mode(self):
+        return g["mode"]
+
+    def wapp_sub_mode(self, mode):
+        self.log("Mode changed to: {}".format(mode))
+        g["mode"] = mode
+
+    def wapp_sub_log(self, log, text):
+        self.find_log(log)["contents"].append(log)
+
+    def wapp_logs_all(self):
+        return g["logs"]
+
+    def wapp_logs_get(self, name):
+        return self.find_log(name)
+
+    def wapp_start(self):
+        self.publish("org.srobo.project.name", g["project"]["name"])
+        self.publish("org.srobo.project.version", g["project"]["version"])
+        self.publish("org.srobo.pyenv.version", g["pyenv"]["version"])
+
+        g["state"] = "booting"
+        self.publish("org.srobo.state", "booting")
+        yield sleep(5)
+        g["state"] = "started"
+        self.publish("org.srobo.state", "started")
+
+        self.log("Robot started.")
+
+    def wapp_stop(self):
+        g["state"] = "stopping"
+        self.publish("org.srobo.state", "stopping")
+        yield sleep(5)
+        g["state"] = "stopped"
+        self.publish("org.srobo.state", "stopped")
+
+        self.log("Robot stopped.")
+
+    def wapp_get_state(self):
+        return g["state"]
+
+    def wapp_get_pyenv_version(self):
+        return g["pyenv"]["version"]
+
+    def wapp_get_project_name(self):
+        return g["project"]["name"]
+
+    def wapp_get_project_version(self):
+        return g["project"]["version"]
+
+    def wapp_power_output_state(self, index, state):
+        g["power_outputs"][index]["state"] = state
 
+    def wapp_power_get_output_state(self, index):
+        return g["power_outputs"][index]["state"]
 
-"""def find_log(name):
-    for log in g["logs"]:
-        if log["name"] == name:
-            return log
+    def wapp_servos_servo_value(self, board, index, value):
+        g["servo_boards"][board]["servos"][index]["value"] = value
 
+    def wapp_servos_get_servo(self, board, index):
+        return g["servo_boards"][board]["servos"][index]
 
-def log(m):
-    wapp.session.publish("org.srobo.logs.append", "current", m)
-    find_log("current")["contents"].append(m)
+    def wapp_servos_get_board(self, serial_number):
+        return g["servo_boards"][serial_number]
 
+    def wapp_servos_all_boards(self):
+        return g["servo_boards"]
 
-@wapp.register("org.srobo.hello")
-def wapp_hello(client_version):
-    compatible = False if random.randint(0, 10) == 0 else True
-    return {"compatible": compatible}
+    def wapp_motors_motor_value(self, board, index, value):
+        g["motor_boards"][board]["motors"][index]["value"] = value
 
+    def wapp_motors_get_motor(self, board, index):
+        return g["motor_boards"][board]["motors"][index]
 
-@wapp.register("org.srobo.zone")
-def wapp_get_zone():
-    return g["zone"]
+    def wapp_motors_get_board(self, serial_number):
+        return g["motor_boards"][serial_number]
 
+    def wapp_motors_all_boards(self):
+        return g["motor_boards"]
 
-@wapp.subscribe("org.srobo.zone")
-def wapp_sub_zone(zone):
-    log("Zone changed to: {}".format(zone))
-    g["zone"] = zone
+    def wapp_ruggeduinos_pin_mode(self, board, index, mode):
+        g["ruggeduinos"][board]["pins"][index]["mode"] = mode
 
+    def wapp_ruggeduinos_pin_value(self, board, index, value):
+        g["ruggeduinos"][board]["pins"][index]["value"] = value
 
-@wapp.register("org.srobo.mode")
-def wapp_get_mode():
-    return g["mode"]
+    def wapp_ruggeduinos_get_pin(self, board, index):
+        return g["ruggeduinos"][board]["pins"][index]
 
+    def wapp_ruggeduinos_get_board(self, serial_number):
+        return g["ruggeduinos"][serial_number]
 
-@wapp.subscribe("org.srobo.mode")
-def wapp_sub_mode(mode):
-    log("Mode changed to: {}".format(mode))
-    g["mode"] = mode
-
-
-@wapp.subscribe("org.srobo.logs.append")
-def wrap_sub_log(log, text):
-    find_log(log)["contents"].append(log)
-
-
-@wapp.register("org.srobo.logs.all")
-def wapp_logs_all():
-    return g["logs"]
-
-
-@wapp.register("org.srobo.logs.get")
-def wapp_logs_get(name):
-    return find_log(name)
-
-
-def sleep(delay):
-    d = twisted.internet.defer.Deferred()
-    twisted.internet.reactor.callLater(delay, d.callback, None)
-    return d
-
-
-@wapp.register("org.srobo.start")
-def wrap_start():
-    wapp.session.publish("org.srobo.project.name", g["project"]["name"])
-    wapp.session.publish("org.srobo.project.version", g["project"]["version"])
-    wapp.session.publish("org.srobo.pyenv.version", g["pyenv"]["version"])
-
-    g["state"] = "booting"
-    wapp.session.publish("org.srobo.state", "booting")
-    yield sleep(5)
-    g["state"] = "started"
-    wapp.session.publish("org.srobo.state", "started")
-
-    log("Robot started.")
-
-
-@wapp.register("org.srobo.stop")
-def wrap_start():
-    g["state"] = "stopping"
-    wapp.session.publish("org.srobo.state", "stopping")
-    yield sleep(5)
-    g["state"] = "stopped"
-    wapp.session.publish("org.srobo.state", "stopped")
-
-    log("Robot stopped.")
-
-
-@wapp.register("org.srobo.state")
-def wapp_get_state():
-    return g["state"]
-
-
-@wapp.register("org.srobo.pyenv.version")
-def wapp_get_pyenv_version():
-    return g["pyenv"]["version"]
-
-
-@wapp.register("org.srobo.project.name")
-def wapp_get_project_name():
-    return g["project"]["name"]
-
-
-@wapp.register("org.srobo.project.version")
-def wapp_get_project_version():
-    return g["project"]["version"]
-
-
-@wapp.subscribe("org.srobo.power.output_state")
-def wapp_power_output_state(index, state):
-    g["power_outputs"][index]["state"] = state
-
-
-@wapp.register("org.srobo.power.get_output_state")
-def wapp_power_get_output_state(index):
-    return g["power_outputs"][index]["state"]
-
-
-@wapp.subscribe("org.srobo.servos.servo_value")
-def wapp_servos_servo_value(board, index, value):
-    g["servo_boards"][board]["servos"][index]["value"] = value
-
-
-@wapp.register("org.srobo.servos.get_servo")
-def wapp_servos_get_servo(board, index):
-    return g["servo_boards"][board]["servos"][index]
-
-
-@wapp.register("org.srobo.servos.get_board")
-def wapp_servos_get_board(serial_number):
-    return g["servo_boards"][serial_number]
-
-
-@wapp.register("org.srobo.servos.all_boards")
-def wapp_servos_all_boards():
-    return g["servo_boards"]
-
-
-@wapp.subscribe("org.srobo.motors.motor_value")
-def wapp_motors_motor_value(board, index, value):
-    g["motor_boards"][board]["motors"][index]["value"] = value
-
-
-@wapp.register("org.srobo.motors.get_motor")
-def wapp_motors_get_motor(board, index):
-    return g["motor_boards"][board]["motors"][index]
-
-
-@wapp.register("org.srobo.motors.get_board")
-def wapp_motors_get_board(serial_number):
-    return g["motor_boards"][serial_number]
-
-
-@wapp.register("org.srobo.motors.all_boards")
-def wapp_motors_all_boards():
-    return g["motor_boards"]
-
-
-@wapp.subscribe("org.srobo.ruggeduinos.pin_mode")
-def wapp_ruggeduinos_pin_mode(board, index, mode):
-    g["ruggeduinos"][board]["pins"][index]["mode"] = mode
-
-
-@wapp.subscribe("org.srobo.ruggeduinos.pin_value")
-def wapp_ruggeduinos_pin_value(board, index, value):
-    g["ruggeduinos"][board]["pins"][index]["value"] = value
-
-
-@wapp.register("org.srobo.ruggeduinos.get_pin")
-def wapp_ruggeduinos_get_pin(board, index):
-    return g["ruggeduinos"][board]["pins"][index]
-
-
-@wapp.register("org.srobo.ruggeduinos.get_board")
-def wapp_ruggeduinos_get_board(serial_number):
-    return g["ruggeduinos"][serial_number]
-
-
-@wapp.register("org.srobo.ruggeduinos.all_boards")
-def wapp_ruggeduinos_all_boards():
-    return g["ruggeduinos"]
-
-
-@wapp.register("org.srobo.battery")
-def wapp_get_battery():
-    return g["battery"]
-
-
-@wapp.register("org.srobo.battery.level")
-def wapp_get_battery_level():
-    return g["battery"]["level"]"""
+    def wapp_ruggeduinos_all_boards(self):
+        return g["ruggeduinos"]
 
 
 ################################################################################
@@ -374,7 +329,8 @@ if __name__ == "__main__":
 
     @crochet.run_in_reactor
     def start_wamp():
-        runner = wamp.ApplicationRunner(url="ws://0.0.0.0:9000", realm="srobo", standalone=True)
+        runner = wamp.ApplicationRunner(url="ws://0.0.0.0:9000", realm="srobo",
+                                        standalone=True)
         runner.run(MyComponent, start_reactor=False)
 
     start_wamp()
